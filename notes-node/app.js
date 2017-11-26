6128532041e48da9d11d1ddaadd0968a5be31ba4
Didac Esteban Andres <didacesteban@gmail.com>
2017-11-26 12:28:31 +0100

    cleaning code to next classes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,6 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-console.log(_.isString(4));
-console.log(_.isString('true'));
-
-// let user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username} You are ${notes.age}`, (err) => {
-//   if(err){
-//     console.log(`Something goes wrong: ${err}`);
-//   }
-// });
