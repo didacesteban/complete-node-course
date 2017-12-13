76c0879e0d64bd9209a54f0000a0216b19f2ccb3
Didac Esteban Andres <didacesteban@gmail.com>
2017-12-13 20:02:34 +0100

    adding class 14

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,5 +2,29 @@ console.log('starting app');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+
+console.log('YARGS ', argv);
+
+var command = argv._[0];
+
+switch (command) {
+  case 'add':
+    notes.addNote(argv.title, argv.body);
+    break;
+  case 'list':
+    notes.getAllNotes();
+    break;
+  case 'read':
+    notes.readNote(argv.title);
+    break;
+  case 'remove':
+    notes.removeNote(argv.title);
+    break;
+  default:
+    console.log('Command not found');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,6 +1,24 @@
 console.log('starting notes js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+var addNote = (title, body) => {
+  console.log('adding note ', title, body);
+};
+
+var getAllNotes = () => {
+  console.log('Getting all notes');
+};
+
+var readNote = (title) => {
+  console.log('Rading note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAllNotes,
+  readNote,
+  removeNote
 };
