258fbf5b191da72888c68799f73f333e5ae95cfc
Didac Esteban Andres <didacesteban@gmail.com>
2017-12-28 11:42:33 +0100

    class 31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      descrive: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+let encodedAddress = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=91%20rambla%20sant%20nebridi%20terrassa',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (err, response, body) => {
-  console.log(body);
+  console.log(`Address lat and lng: ${body.results[0].geometry.location.lat}, ${body.results[0].geometry.location.lng}`);
 });
