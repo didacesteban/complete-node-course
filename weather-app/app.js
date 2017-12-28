2525c7fc60745e9e27ebf373d6a2a38aa0e67fce
Didac Esteban Andres <didacesteban@gmail.com>
2017-12-28 12:44:05 +0100

    catching errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -20,5 +20,11 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (err, response, body) => {
-  console.log(`Address lat and lng: ${body.results[0].geometry.location.lat}, ${body.results[0].geometry.location.lng}`);
+  if(err) {
+    console.log('Unable to connect to Google Servers.');
+  } else if(body.status === 'ZERO_RESULTS'){
+    console.log('Invalid address');
+  } else if(body.status === 'OK'){
+    console.log(`Address lat and lng: ${body.results[0].geometry.location.lat}, ${body.results[0].geometry.location.lng}`);
+  }
 });
