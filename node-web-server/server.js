c56fa251e9b9403c31ec12fca2ed46715f2dcbc8
Didac Esteban Andres <didacesteban@gmail.com>
2017-12-29 19:23:39 +0100

    class 44

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  //res.send('hello express!');
-  res.send({
-    name: 'Didac',
-    likes: ['Girls', 'Money']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my page'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('about page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
